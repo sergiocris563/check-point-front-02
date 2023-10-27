979a765c4d3b9cbdcf3a32ce2a9ee67bae8e50aa
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:16:11 -0300

    Adicionando commits

---
+++ animals.js
@@ -0,0 +1,8 @@
+// Código para o arquivo animals.js
+const animals = ["macaco", "tucano", "elefante", "pavão", "hipopótamo", "leão"];
+
+if (!animals.includes("leão")) {
+  console.log("Leão não existe no array animals.");
+} else {
+  console.log("Existe um leão no array animals.");
+}
---
+++ filmes.js
@@ -0,0 +1,3 @@
+const my3FavoriteTVShows = ["Sintonia", "DoctorWho", "A festa da salsicha"];
+
+console.log(my3FavoriteTVShows.length === 3);
